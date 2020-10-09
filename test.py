39877fbfbe77fad208eb80641ae8d2a6b88f23d9
chunhoi wong <jordanwong1202@yahoo.com.hk>
2020-10-09 16:03:40 -0700

    save the new code

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-if _name_ == '_main_':
-	x="ECE_180_DA_DB"
-	if x=="EE_180_DA_DB":
-	  print("You are living in 2017")
+if __name__ == '__main__':
+#this is new test.py 10/9/2020
+	x = "ECE_180_DA_DB"
+	if x == "ECE_180_DA_DB":
+		print("You are living in 2020")
 	else:
-	  #this is a comment
-	  x = x+ " - Best class ever"
-	  print(x)
+		#this is a comment
+		x = x + " - Best class ever"
+		print(x)
